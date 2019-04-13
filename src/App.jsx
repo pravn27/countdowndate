0d0779102b89aee7445418e7bea883ab1761b6d4
pravn27 <praveen.sri27@gmail.com>
2019-04-13 20:13:55 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      deadline: "December 25, 2019",
+      deadline: "December 25, 2020",
       newDeadline: "",
       errorDisplay: false
     }
